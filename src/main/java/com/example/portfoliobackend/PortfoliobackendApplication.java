838b44187f85fcddacf63d3cd57827aae965a9ba
Rajeev Singh <rajeevsingh@gmail.com>
2026-06-06 01:54:02 +0530

    Backend Properly Running

--- src/main/java/com/example/portfoliobackend/PortfoliobackendApplication.java
+++ src/main/java/com/example/portfoliobackend/PortfoliobackendApplication.java
@@ -8,6 +8,5 @@ public class PortfoliobackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfoliobackendApplication.class, args);
-		System.out.println("Application Started");
 	}
 }
