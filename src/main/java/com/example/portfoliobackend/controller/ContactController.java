c4ecfccac837a9134460f4db958305a68487eb89
Rajeev Singh <rajeevsingh@gmail.com>
2026-06-06 03:27:38 +0530

    Allow CORS

--- src/main/java/com/example/portfoliobackend/controller/ContactController.java
+++ src/main/java/com/example/portfoliobackend/controller/ContactController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ContactController {
 
     @Autowired
